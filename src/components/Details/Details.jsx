5d1c845e59f01fb9043a2368ecafda64db8256d3
Anton Lysenko <antonlysenko161@yandex.ru>
2025-09-28 21:34:14 +0300

    Внес правки по заданию

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,11 +1,11 @@
 import './Details.css'
 
 function Details({ userDetails }) {
-    const { details, name} = userDetails;
+    const { details, name, avatar} = userDetails;
 
     return (
         <div className="details">
-            <img src='https://i.ytimg.com/vi/jg8ixdQzrjc/maxresdefault.jpg' alt="avatar" className="img" />
+            <img src={avatar} alt="avatar" className="img" />
             <div className="name">{name}</div>
             <div className="city">City: {details.city}</div>
             <div className="company">Company: {details.company}</div>
